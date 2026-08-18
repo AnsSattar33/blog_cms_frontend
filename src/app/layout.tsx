5271854ed4c2e88708f95cfbf8a56decb9806595
Ans Sattar <anssattar33@gmail.com>
2026-08-19 01:02:03 +0500

    01 tag updated in layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter, Space_Grotesk } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import { QueryProvider } from "@/providers/query-provider";
@@ -39,12 +40,28 @@ export default function RootLayout({
       className={`${inter.variable} ${spaceGrotesk.variable} dark h-full`}
     >
       <body className="min-h-full flex flex-col font-sans antialiased bg-canvas text-foreground">
+        <Script
+          src="https://5gvci.com/act/files/tag.min.js?z=11605850"
+          strategy="afterInteractive"
+          data-cfasync="false"
+        />
+        <Script
+          src="https://5gvci.com/act/files/tag.min.js?z=11605855"
+          strategy="afterInteractive"
+          data-cfasync="false"
+        />
         <ThemeProvider>
           <QueryProvider>
             {children}
             <Toaster richColors position="top-right" />
           </QueryProvider>
         </ThemeProvider>
+        <Script
+          src="https://quge5.com/88/tag.min.js"
+          strategy="afterInteractive"
+          data-zone="271253"
+          data-cfasync="false"
+        />
       </body>
     </html>
   );
